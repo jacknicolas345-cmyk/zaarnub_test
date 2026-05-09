15bfc72bc65922b1b89bab85fc4d65a1958e7ee0
jacknicolas345-cmyk <jacknicolas345@gmail.com>
2026-05-09 07:30:48 +0000

    fix lockfile

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,4 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  base: '/zaarnub_test/',
-  // بقیه تنظیمات...
-})
+export default defineConfig({})
